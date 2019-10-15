db6801d840b56a57ab3b787d570cb2e59085b49c
JohnHake <john.hake@jetbrains.com>
2019-10-15 08:20:46 -0700

    Kotlin conversion

---
+++ androidstudio_basics/src/main/kotlin/com/intellij/sdk/androidstudio/PopupDialogAction.kt
@@ -0,0 +1,46 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package com.intellij.sdk.androidstudio
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import icons.SdkIcons
+
+/**
+ * Action class to demonstrate how to interact with the IntelliJ Platform.
+ * The only action this class performs is to provide the user with a popup dialog as feedback.
+ * Typically this class is instantiated by the IntelliJ Platform framework based on declarations
+ * in the plugin.xml file. But when added at runtime this class is instantiated by an action group.
+ */
+class PopupDialogAction : AnAction() {
+
+    /**
+     * Gives the user feedback when the dynamic action menu is chosen.
+     * Pops a simple message dialog. See the psi_demo plugin for an
+     * example of how to use AnActionEvent to access data.
+     * @param e Event received when the associated menu item is chosen.
+     */
+    override fun actionPerformed(e: AnActionEvent) {
+        val proj = e.project
+        Messages.showMessageDialog(
+            proj,
+            "Popup dialog action",
+            "Greetings from Android Studio Basics Plugin",
+            SdkIcons.Sdk_default_icon
+        )
+    }
+
+    /**
+     * Determines whether this menu item is available for the current context.
+     * Requires a project to be open.
+     * @param e Event received when the associated group-id menu is chosen.
+     */
+    override fun update(e: AnActionEvent) {
+        // Set the availability based on whether a project is open
+        val project = e.project
+        e.presentation.isEnabledAndVisible = project != null
+    }
+
+}
