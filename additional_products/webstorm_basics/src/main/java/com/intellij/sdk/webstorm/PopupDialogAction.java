b2cb073d19b36a7c6a45694c4f6a49bce57dfa86
JohnHake <john.hake@jetbrains.com>
2019-11-03 22:38:10 -0800

    Cleanup

--- additional_products/webstorm_basics/src/main/java/com/intellij/sdk/webstorm/PopupDialogAction.java
+++ additional_products/webstorm_basics/src/main/java/com/intellij/sdk/webstorm/PopupDialogAction.java
@@ -1,6 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package com.intellij.sdk.pycharm;
+package com.intellij.sdk.webstorm;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
