927b9c0175576cdfaa275fcbbfbaed1751d884bf
JohnHake <john.hake@jetbrains.com>
2020-01-09 17:49:48 -0800

    For custom language

--- SimpleTest/src/Test.java
+++ SimpleTest/src/Test.java
@@ -1,10 +1,5 @@
 public class Test {
-  public static void main(String[] args) {
-    
-    System.out.println("simple:website");
-  
-    int abc = 5;
-  
-    System.out.println(String.format("This has no keys or properties, just %d", abc));
-  }
+    public static void main(String[] args) {
+        System.out.println("simple:website");
+    }
 }
